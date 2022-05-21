03172db947b23be8cc61a44a07c49aae1a97730d
Gustavo Voltolini <gustavo.voltolini@ambevtech.com.br>
2022-05-21 01:43:25 -0300

    Fixing views and templates

--- app/views/templates_views.py
+++ app/views/templates_views.py
@@ -8,19 +8,19 @@ app_views = Blueprint('views', __name__)
 # Templates
 @app_views.route('/', methods=['GET'])
 def home_view():
-    _json = request.get_json()
+    # _json = request.get_json()
     return render_template('index.html')
 
 
 @app_views.route('/how_to_recycle', methods=['GET'])
 def how_to_recycle_view():
-    _json = request.get_json()
+    # _json = request.get_json()
     return render_template('index.html')
 
 
 @app_views.route('/where_recycle', methods=['GET'])
 def where_recycle_view():
-    _json = request.get_json()
+    # _json = request.get_json()
     return render_template('index.html')
 
 
