9a087263947738a95f8197b68b107ceb5ea9b6b5
Gustavo Voltolini <gustavo.voltolini@ambevtech.com.br>
2022-05-21 17:06:09 -0300

    Creating type_of_garage model

--- app/models/type_of_garbage.py
+++ app/models/type_of_garbage.py
@@ -1,21 +1,9 @@
-from uuid import uuid4
-from database import db
 
 
-class TypeOfGarbage(db.Model):
-    __tablename__ = 'type_of_garbage'
+class TypeOfGarbage:
 
-    # Columns
-    id = db.Column(db.String(36), default=lambda: str(uuid4()), primary_key=True)
-    type = db.Column(db.String(128), nullable=False, unique=True)
-    description = db.Column(db.String(128), nullable=True)
-    color = db.Column(db.String(128), nullable=True)
-    active = db.Column(db.Boolean(), default=True)
-
-    def serialize(self):
-        return {
-                'id': self.id,
-                'type': self.type,
-                'color': self.color,
-                'active': self.active
-                }
+    plastic = "P"
+    glass = "G"
+    organic = "O"
+    paper = "P"
+    metal = "M"
