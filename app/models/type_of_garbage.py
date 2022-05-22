eb33fda0f4d1ae3eace8ed49eed4b9a1ec35cbe8
Gustavo Voltolini <gustavo.voltolini@ambevtech.com.br>
2022-05-21 21:32:57 -0300

    Creating Type_of_garbage

--- app/models/type_of_garbage.py
+++ app/models/type_of_garbage.py
@@ -2,8 +2,13 @@
 
 class TypeOfGarbage:
 
-    plastic = "P"
-    glass = "G"
-    organic = "O"
-    paper = "P"
-    metal = "M"
+    plastic = {"name": "Plastic", "color": "red"}
+    glass = {"name": "Glass", "color": "green"}
+    organic = {"name": "Organic", "color": "brown"}
+    paper = {"name": "Paper", "color": "blue"}
+    metal = {"name": "Metal", "color": "yellow"}
+
+    def get_type(self, type_name):
+        if self.__getattribute__(type_name):
+            return self.__getattribute__(type_name)
+        return {}
